fd9e7422e3395ba62b2235639e7a0b1d31268e95
jelmy hermosilla <jelmyhermosilla@MacBook-Pro-de-jelmy.local>
2021-05-04 12:26:24 -0400

    acs_yg_gini_pipeline

---
+++ acs/etl/acs_yg_gini_pipeline.py
@@ -0,0 +1,71 @@
+import requests
+import numpy as np
+import pandas as pd
+
+from bamboo_lib.connectors.models import Connector
+from bamboo_lib.models import EasyPipeline, PipelineStep, Parameter
+from bamboo_lib.steps import DownloadStep, LoadStep
+
+from acs.static import FIPS_CODE, LIST_STATE, DICT_APIS
+from acs.helper import read_by_zone, create_geoid_in_df
+
+class TransformStep(PipelineStep):
+    def run_step(self, prev, params):
+        year = params.get('year')
+        estimate = params.get('estimate')
+        apis = DICT_APIS['acs_yg_gini']
+
+        def transform_by_zone(year, geo, estimate, apis):
+            df = read_by_zone(year, geo, estimate, apis)
+            df = create_geoid_in_df(df, geo)
+            df.rename(columns = {'B19083_001E': 'moe', 'B19083_001M': 'mea'}, inplace=True)
+            df['year'] = year
+            df = df[['moe', 'mea', 'year', 'geoid']]
+            return df
+
+        df_final = pd.DataFrame()
+
+        list_geo = ['us', 'state', 'county', 'place', 'public use microdata area', 'metropolitan statistical area/micropolitan statistical area', 'congressional district']
+
+        for zone in list_geo:
+            df_geo = transform_by_zone(year, zone, estimate, apis)
+            df_final = df_final.append(df_geo).reset_index(drop=True)
+
+        print(df_final.head())
+        return df_final
+
+class AcsYgGiniPipeline(EasyPipeline):
+    @staticmethod
+    def parameter_list():
+        return[
+            Parameter(label='year', name='year', dtype=str),
+            Parameter(label='estimate', name='estimate', dtype=str),
+            Parameter(label='server', name='server', dtype=str)
+        ]
+
+    @staticmethod
+    def steps(params):
+        db_connector = Connector.fetch(params.get('server'), open('../../conns.yaml'))
+
+        dtype = {
+            'year': 'smallint',
+            'moe': 'float',
+            'mea': 'float',
+            'geoid': 'text'
+        }
+
+        transform_step = TransformStep()
+
+        load_step = LoadStep(
+            "acs_yg_gini_{}".format(params.get('estimate')), db_connector, if_exists = 'append',
+            schema= 'acs', dtype = dtype, pk = ['geoid']
+        )
+
+        return [transform_step, load_step]
+
+if __name__ == '__main__':
+    acs_pipeline = AcsYgGiniPipeline()
+    for year in range(2014, 2021 + 1):
+        acs_pipeline.run({
+            'year': year
+        })
---
+++ acs/static.py
@@ -0,0 +1,20 @@
+LIST_STATE = ['28', '29', '30', '31', '32', '33', '34', '35', '36', '37', '38', '39', '40', '41', '42', '44', '45', '46', '47',
+              '48', '50', '49', '51', '53', '54', '55', '56', '72', '01', '02', '04', '05', '06', '08', '10', '11', '09', '12', 
+              '13', '16', '15', '17', '18', '19', '20', '21', '22', '23', '24', '25', '26', '27']
+
+FIPS_CODE = {
+    'us': '01000US',
+    'state': '04000US',
+    'county': '05000US',
+    'place': '16000US',
+    'public use microdata area': '79500US',
+    'msa': '31000US',
+    'tract': '14000US',
+    'congressional district': '50000US',
+    'zip code tabulation area': '86000US'
+}
+
+DICT_APIS = {
+    'acs_yg_gini': ['https://api.census.gov/data/{}/acs/acs{}?get=B19083_001E,B19083_001M&for={}&key=d0a3481f70837de29a56f7a3b7df087007e942e1', 
+                    'https://api.census.gov/data/{}/acs/acs{}?get=B19083_001E,B19083_001M&for={}&key=d0a3481f70837de29a56f7a3b7df087007e942e1&in=state:{}']
+}
