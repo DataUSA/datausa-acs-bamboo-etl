1843758aab30420c2993529fb129d2652c1ca8f0
jelmy hermosilla <jelmyhermosilla@MacBook-Pro-de-jelmy.local>
2021-05-11 14:35:34 -0400

    add new pipeline in init

--- acs/init.sh
+++ acs/init.sh
@@ -5,3 +5,7 @@ bamboo-cli --folder acs_health_coverage_s --entry acs_health_coverage_s_pipeline
 #acs_yg_gini
 bamboo-cli --folder acs_yg_gini --entry acs_yg_gini_pipeline --year=2019 --estimate='1' --server=monet-backend
 bamboo-cli --folder acs_yg_gini --entry acs_yg_gini_pipeline --year=2019 --estimate='5' --server=monet-backend
+
+#acs_yg_household_income
+bamboo-cli --folder acs_yg_household_income --entry acs_yg_household_income_pipeline --year=2019 --estimate='1' --server=monet-backend
+bamboo-cli --folder acs_yg_household_income --entry acs_yg_household_income_pipeline --year=2019 --estimate='5' --server=monet-backend
