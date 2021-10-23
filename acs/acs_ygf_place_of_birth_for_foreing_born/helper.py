79d5393b0b494b428418762f9b6bb78089a3a2dc
innerstage <mbarbierif@gmail.com>
2021-10-23 03:57:30 -0300

    etl fix 4

--- acs/acs_ygf_place_of_birth_for_foreing_born/helper.py
+++ acs/acs_ygf_place_of_birth_for_foreing_born/helper.py
@@ -1,35 +1,35 @@
 def add_cols_2019(df):
     if "B05006_099E" in df.columns:
         df["B05006_099E"] = df["B05006_097E"] + df["B05006_098E"] + df["B05006_099E"]
-        df.drop(columns=["B05006_097E","B05006_098E"])
+        df = df.drop(columns=["B05006_097E","B05006_098E"])
 
     if "B05006_099M" in df.columns:
         df["B05006_099M"] = df["B05006_097M"] + df["B05006_098M"] + df["B05006_099M"]
-        df.drop(columns=["B05006_097M","B05006_098M"])
+        df = df.drop(columns=["B05006_097M","B05006_098M"])
 
     if "B05006_104E" in df.columns:
         df["B05006_104E"] = df["B05006_102E"] + df["B05006_103E"] + df["B05006_104E"]
-        df.drop(columns=["B05006_102E","B05006_103E"])
+        df = df.drop(columns=["B05006_102E","B05006_103E"])
 
     if "B05006_104M" in df.columns:
         df["B05006_104M"] = df["B05006_102M"] + df["B05006_103M"] + df["B05006_104M"]
-        df.drop(columns=["B05006_102M","B05006_103M"])
+        df = df.drop(columns=["B05006_102M","B05006_103M"])
 
     if "B05006_120E" in df.columns:
         df["B05006_120E"] = df["B05006_118E"] + df["B05006_120E"]
-        df.drop(columns=["B05006_118E"])
+        df = df.drop(columns=["B05006_118E"])
 
     if "B05006_120M" in df.columns:
         df["B05006_120M"] = df["B05006_118M"] + df["B05006_120M"]
-        df.drop(columns=["B05006_118M"])
+        df = df.drop(columns=["B05006_118M"])
 
     if "B05006_128E" in df.columns:
         df["B05006_128E"] = df["B05006_127E"] + df["B05006_128E"]
-        df.drop(columns=["B05006_127E"])
+        df = df.drop(columns=["B05006_127E"])
 
     if "B05006_128M" in df.columns:
         df["B05006_128M"] = df["B05006_127M"] + df["B05006_128M"]
-        df.drop(columns=["B05006_127M"])
+        df = df.drop(columns=["B05006_127M"])
 
     return df
 
