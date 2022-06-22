e554433b1a0527ee8b9a35c4ce5cec362f23929a
nicolasnetz <nnetz2018@udec.cl>
2022-06-22 15:48:41 -0400

    exception for 2020 estimate 1

--- acs/acs_ygsi_gender_by_industry_c/acs_ygsi_gender_by_industry_c_pipeline.py
+++ acs/acs_ygsi_gender_by_industry_c/acs_ygsi_gender_by_industry_c_pipeline.py
@@ -105,9 +105,12 @@ class AcsYgsiGenderByIndustryCPipeline(EasyPipeline):
 if __name__ == '__main__':
     acs_pipeline = AcsYgsiGenderByIndustryCPipeline()
     for estimate in ['1', '5']:
-        for year in range(2013, 2019 + 1):
-            acs_pipeline.run({
-                'year': year,
-                'estimate': estimate,
-                'server': 'monet-backend'
-            })
+        for year in range(2013, 2020 + 1):
+            if year == 2020 and estimate == "1":
+                continue
+            else:
+                acs_pipeline.run({
+                    'year': year,
+                    'estimate': estimate,
+                    'server': 'monet-backend'
+                })
