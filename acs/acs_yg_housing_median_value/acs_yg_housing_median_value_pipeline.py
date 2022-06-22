a5b7c09ad06d991a9684bf014e256bbd6d7f0efc
nicolasnetz <nnetz2018@udec.cl>
2022-06-22 12:56:33 -0400

    exception for 2020 estimate in housing median value

--- acs/acs_yg_housing_median_value/acs_yg_housing_median_value_pipeline.py
+++ acs/acs_yg_housing_median_value/acs_yg_housing_median_value_pipeline.py
@@ -73,8 +73,11 @@ if __name__ == '__main__':
     acs_pipeline = AcsYgHousingMedianValuePipeline()
     for estimate in ['1', '5']:
         for year in range(2013, 2019 + 1):
-            acs_pipeline.run({
-                'year': year,
-                'estimate': estimate,
-                'server': 'monet-backend'
-            })
+            if year == 2020 and estimate == "1":
+                continue
+            else:
+                acs_pipeline.run({
+                    'year': year,
+                    'estimate': estimate,
+                    'server': 'monet-backend'
+                })
