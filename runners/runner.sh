2731cb29b5aa5df36756cd4a8c0ec4d953e9cebf
Nicolas <nicolasnetz@github.com>
2023-07-25 14:31:09 +0000

    add parameters to script

--- runners/runner.sh
+++ runners/runner.sh
@@ -1,10 +1,27 @@
 #!/bin/bash
-export DATAUSA_PATH="/code/datausa/datausa-acs-bamboo-etl";
-mkdir -p $DATAUSA_PATH/runners/logs;
-export SERVERS=("monet-backend" "postgres-zcube")
+
+# Parse arguments (3)
+if [ $# -ne 3 ]; then
+  echo "Usage: $0 <DATAUSA_PATH> <SERVER> <YEAR>"
+  exit 1
+fi
+
+# Assign arguments to variables
+export DATAUSA_PATH="$1" # ex. /code/datausa/datausa-acs-bamboo-etl
+export SERVERS="$2" # ex. postgres-zcube or monet-backend
+export YEAR="$3" # ex. 2021
+
+
+export SERVERS=("$2")
 export ESTIMATES=("1" "5")
-export PP_YEAR="2021";
+export PP_YEAR=$YEAR;
 export PYTHON_FILES=$(find "$DATAUSA_PATH/acs" -type f -name "*_pipeline.py")
+
+mkdir -p $DATAUSA_PATH/runners/logs;
+for SERVER in "${SERVERS[@]}"; do
+    mkdir -p $DATAUSA_PATH/runners/logs/$SERVER/1/$PP_YEAR;
+    mkdir -p $DATAUSA_PATH/runners/logs/$SERVER/5/$PP_YEAR;
+done
 
 for SERVER in "${SERVERS[@]}"; do
     for ESTIMATE in "${ESTIMATES[@]}"; do
@@ -14,7 +31,7 @@ for SERVER in "${SERVERS[@]}"; do
             export PIPELINE_NAME=${FILE_NAME%%_pipeline}
             echo Running-$PIPELINE_NAME;
             echo ;
-            screen -S "$PIPELINE_NAME-$SERVER-$ESTIMATE" -dm -L -Logfile "$DATAUSA_PATH/runners/logs/$PIPELINE_NAME-$SERVER-$ESTIMATE.log" bash -c "export PIPELINE_NAME=$PIPELINE_NAME;export SERVER=$SERVER;export ESTIMATE=$ESTIMATE;export PP_YEAR=$PP_YEAR;export FILE_NAME=$FILE_NAME;source $DATAUSA_PATH/venv/bin/activate;source $DATAUSA_PATH/.env;cd $PIPELINE_DIR;bamboo-cli --folder . --entry $FILE_NAME --year=$PP_YEAR --estimate=$ESTIMATE --server=$SERVER;";
+            screen -S "$PIPELINE_NAME-$SERVER-$ESTIMATE" -L -Logfile "$DATAUSA_PATH/runners/logs/$SERVER/$ESTIMATE/$PP_YEAR/$PIPELINE_NAME-$SERVER-$ESTIMATE.log" bash -c "export PIPELINE_NAME=$PIPELINE_NAME;export SERVER=$SERVER;export ESTIMATE=$ESTIMATE;export PP_YEAR=$PP_YEAR;export FILE_NAME=$FILE_NAME;source $DATAUSA_PATH/venv/bin/activate;source $DATAUSA_PATH/.env;cd $PIPELINE_DIR;bamboo-cli --folder . --entry $FILE_NAME --year=$PP_YEAR --estimate=$ESTIMATE --server=$SERVER;";
             echo Done Running-$PIPELINE_NAME;
         done
     done
