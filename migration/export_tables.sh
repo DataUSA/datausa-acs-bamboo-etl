3c977ae5a4ab4a409b483219e9eb38ba406cabc0
hermosillajelmy <jelmyhermosilla@gmail.com>
2025-04-01 19:43:02 -0300

    add script for ingest data in clickhouse

--- migration/export_tables.sh
+++ migration/export_tables.sh
@@ -5,11 +5,13 @@ query_read="SELECT name
 FROM sys.tables
 WHERE schema_id = (SELECT id FROM sys.schemas WHERE name = 'acs');"
 
-mclient -d datausa -s "$query_read" > temp_file.txt
-grep "|" temp_file.txt | awk -F '|' '{print $2}' > clean_tables.txt
+mclient -d datausa -u monetdb -s "$query_read" > temp_file.txt
+grep "|" temp_file.txt | awk -F '|' '{gsub(/^[ \t]+|[ \t]+$/, "", $2); print $2}' > clean_tables.txt
 
-DEST_DIR=$0
-TABLES_FILE="temp_file.txt"
+export MONETDB_PASSWD="monetdb"
+
+DEST_DIR="/home/deploy/backup"
+TABLES_FILE="clean_tables.txt"
 
 # Create destination directory if it doesn't exist
 mkdir -p "$DEST_DIR"
@@ -22,23 +24,22 @@ fi
 
 # Create temporary directory for CSV files
 TEMP_DIR="$DEST_DIR/temp_csv"
-mkdir -p "$TEMP_DIR"
 
 # Export each table to CSV
 while IFS= read -r table; do
-    echo "Exporting table: $table"
+    echo "Exporting table: $table .."
     
     # Export table to CSV using monetdb
-    mclient -u monetdb -d datausa  -p $DATAUSA_DB_PW -s "COPY SELECT * FROM acs.$table INTO '$TEMP_DIR/${table}.csv' USING DELIMITERS ',' '\n' '\"' NULL AS '\N';"
+    mclient -u monetdb -d datausa -s "COPY SELECT * FROM acs.$table INTO '$TEMP_DIR/${table}.csv' USING DELIMITERS ',' '\n' '\"' NULL AS '\N';"
     
     # Compress the CSV file
-    gzip "$TEMP_DIR/${table}.csv"
+    sudo gzip "$TEMP_DIR/${table}.csv"
 done < "$TABLES_FILE"
 
 # Compress the entire temporary directory
-tar -czf "$DEST_DIR/tables_acs_backup.tar.gz" -C "$TEMP_DIR" .
+sudo tar -czf "$DEST_DIR/tables_acs_backup.tar.gz" -C "$TEMP_DIR" .
 
 # Clean up temporary files
-rm -rf "$TEMP_DIR"
+#rm -rf "$TEMP_DIR"
 
 echo "Process completed. Compressed files are located at: $DEST_DIR/tables_acs_backup.tar.gz" 
---
+++ migration/ingest_tables.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+table=$1
+
+# Check if a table name is passed as an argument
+if [ -z "$table" ]; then
+  echo "Error: No table name specified."
+  exit 1
+fi
+
+# Path to the compressed and uncompressed file
+csv_file="/home/deploy/backups/acs/${table}.csv"
+csv_gz_file="/home/deploy/backups/acs/${table}.csv.gz"
+
+# Step 1: Unzip the .csv.gz file
+echo "Unzipping $csv_gz_file ..."
+sudo gzip -d "$csv_gz_file"
+
+# Check if the unzip was successful
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to unzip the file."
+  exit 1
+fi
+
+# Step 2: Ingest data into ClickHouse
+echo "Ingesting data into table $table ..."
+cat "$csv_file" | clickhouse-client --query="INSERT INTO $table FORMAT CSVWithNames"
+
+# Check if the ingestion was successful
+if [ $? -ne 0 ]; then
+  echo "Error: Failed to ingest data into ClickHouse."
+  exit 1
+fi
+
+echo "Process completed for table $table."
