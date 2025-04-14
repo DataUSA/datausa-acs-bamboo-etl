da7880656518e109f4e85bf938e16ddbdab6eeba
Felipe Ramos <framosmorale@student.unimelb.edu.au>
2025-04-14 11:10:45 -0400

    batch 1

--- acs/acs_ygt_travel_time_to_work/acs_ygt_travel_time_to_work_pipeline.py
+++ acs/acs_ygt_travel_time_to_work/acs_ygt_travel_time_to_work_pipeline.py
@@ -79,18 +79,18 @@ class AcsYgtTravelTimeToWorkPipeline(EasyPipeline):
         db_connector = Connector.fetch(params.get('server'), open('../../conns.yaml'))
         
         dtype = {
-            'year': 'smallint',
-            'moe': 'float',
-            'mea': 'float',
-            'dim_0': 'int',
-            'geoid': 'text'
+            'year': 'UInt16',
+            'moe': 'Float32',
+            'mea': 'Float32',
+            'dim_0': 'UInt8',
+            'geoid': 'String'
         }
 
         transform_step = TransformStep()
 
         load_step = LoadStep(
             "acs_ygt_travel_time_to_work_{}".format(params.get('estimate')), db_connector, if_exists='append',
-            schema='acs', dtype=dtype, pk=['geoid', 'dim_0'], nullable_list=['mea', 'moe']
+            schema='acs', dtype=dtype, pk=['geoid', 'year'], nullable_list=['mea', 'moe']
         )
 
         return [transform_step, load_step]
@@ -102,5 +102,5 @@ if __name__ == '__main__':
             acs_pipeline.run({
                 'year': year,
                 'estimate': estimate,
-                'server': 'monet-backend'
+                'server': 'clickhouse-database'
             })
